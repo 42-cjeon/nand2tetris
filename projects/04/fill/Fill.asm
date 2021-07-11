f1711cfcb1fb935ef4b964e7781a5fd4104de1db
전창민 <anduuin@anduuin-MacBookAir.local>
2021-07-11 17:51:08 +0900

    fill.asm

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,54 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+// KBD = 24576 ~ 24727
+// SCREEN = 16384 ~ 24575
+@24576
+D=A
+@ScreenEnd
+M=D
+@SCREEN
+D=A
+@ScreenCurrentPtr
+M=D
+
+(START)
+    @KBD
+    D=M
+    @SCREENCLR
+    D;JEQ
+    @SCREENSET
+    D;JGT
+
+(SCREENCLR)
+    @ScreenTargetColor
+    M=0
+    @SCREENLOOP
+    0;JMP
+(SCREENSET)
+    @ScreenTargetColor
+    M=-1
+    @SCREENLOOP
+    0;JMP
+
+(SCREENLOOP)
+    @ScreenEnd
+    D=M
+    @ScreenCurrentPtr
+    D=D-M
+    @SCREENLOOPEND
+    D;JEQ
+    @ScreenTargetColor
+    D=M
+    @ScreenCurrentPtr
+    A=M
+    M=D
+    @ScreenCurrentPtr
+    M=M+1
+    @SCREENLOOP
+    0;JMP
+
+(SCREENLOOPEND)
+    @START
+    0;JMP
